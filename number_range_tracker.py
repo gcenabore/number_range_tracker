f34d3ff0fdb46390766527470ea27f0c38fedcb6
gcenabore <gianenabore30@gmail.com>
2024-11-01 00:03:19 +0800

    pseudocode and plans for the program

---
+++ number_range_tracker.py
@@ -0,0 +1,10 @@
+#initialize counts for each range (1-10, 11-20, 21-30, 31-40, 41-50)
+#start an infinite loop using while loop to get the number input
+#ask the user for input
+#make a valid instance for the number input (only digits)
+#use if statement to check if the number is whithin the valid range
+#adjust the count according to the range
+#break the loop if the number is out of range
+#print error if the number input is invalid
+#print the results
+
